c8192059861a4d489329b8873864d6d18283b90e
iFlameing <ialokkumarsingh0@gmail.com>
2020-04-04 20:30:28 +0530

    fixing

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 
 // Start the server
 const port = process.env.PORT || 8080;
-app.listen(port);
+app.listen(process.env.PORT || 3000);
 console.log(`Server listening at ${port}`);
