bbbe97baac38f5c3a5c9b5407ad591b6e9840d26
iFlameing <ialokkumarsingh0@gmail.com>
2020-04-04 21:16:09 +0530

    adding cors for all

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ mongoose.connect(
 );
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
